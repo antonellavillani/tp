ec19b752725b73b35749b6bc5645180bfd418b06
Antonella Villani <115169494+antonellavillani@users.noreply.github.com>
2022-11-14 13:25:05 -0300

    feat: add project structure

---
+++ src/MedioDeTransporte.java
@@ -0,0 +1,79 @@
+import java.util.ArrayList;
+
+public class MedioDeTransporte {
+    private ArrayList<ObjetoTransportable> transportables;
+    private double capacidadMaxima;
+    private double capacidad;
+    private Dimension dimension;
+    private int cantidadMaxima;
+
+    //para usar el arraylist y que no tire error: crear un constructor
+
+    MedioDeTransporte(
+            double capacidadMaxima,
+            Dimension dimension,
+            int cantidadMaxima
+            ){
+        setTransportables(new ArrayList<ObjetoTransportable>());
+        setCapacidad(0);
+        setCapacidadMaxima(capacidadMaxima);
+        setCapacidadMaxima(capacidadMaxima);
+        setDimension(dimension);
+    }
+
+    MedioDeTransporte(
+            double capacidadMaxima,
+            Dimension dimension,
+            int cantidadMaxima,
+            double capacidad
+    ){
+        setTransportables(new ArrayList<ObjetoTransportable>());
+        setCapacidad(capacidad);
+        setCapacidadMaxima(capacidadMaxima);
+        setCapacidadMaxima(capacidadMaxima);
+        setDimension(dimension);
+    }
+
+
+
+    public ArrayList<ObjetoTransportable> getTransportables() {
+        return transportables;
+    }
+
+    public void setTransportables(ArrayList<ObjetoTransportable> transportables) {
+        this.transportables = transportables;
+    }
+
+    public double getCapacidadMaxima() {
+        return capacidadMaxima;
+    }
+
+    public void setCapacidadMaxima(double capacidadMaxima) {
+        this.capacidadMaxima = capacidadMaxima;
+    }
+
+    public double getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(double capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public Dimension getDimension() {
+        return dimension;
+    }
+
+    public void setDimension(Dimension dimension) {
+        this.dimension = dimension;
+    }
+
+    public int getCantidadMaxima() {
+        return cantidadMaxima;
+    }
+
+    public void setCantidadMaxima(int cantidadMaxima) {
+        this.cantidadMaxima = cantidadMaxima;
+    }
+
+}
---
+++ src/ObjetoTransportable.java
@@ -0,0 +1,50 @@
+public class ObjetoTransportable {
+    private double peso;
+    private Dimension dimension;
+    private String name;
+    private boolean fragil;
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public Dimension getDimension() {
+        return dimension;
+    }
+
+    public void setDimension(Dimension dimension) {
+        this.dimension = dimension;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isFragil() {
+        return fragil;
+    }
+
+    public void setFragil(boolean fragil) {
+        this.fragil = fragil;
+    }
+
+    ObjetoTransportable(
+            String nombre,
+            double peso,
+            Dimension dimension,
+            boolean fragil
+    ){
+        setName(nombre);
+        setPeso(peso);
+        setDimension(dimension);
+        setFragil(fragil);
+    }
+}
